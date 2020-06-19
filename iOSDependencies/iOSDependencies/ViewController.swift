fca74abc4b2ebee44893ff12ec10b5d093b55d21
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2020-06-19 09:59:25 -0300

    Add initial project for iOS

---
+++ iOSDependencies/iOSDependencies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDependencies
+//
+//  Created by Rafael Ferreira on 6/19/20.
+//  Copyright © 2020 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
